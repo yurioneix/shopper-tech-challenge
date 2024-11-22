1c1f72af870a230fa24d3567ae4fbe8cec59fad2
yurioneix <yurioneix18@gmail.com>
2024-11-21 23:48:18 -0300

    Cria pasta usecase e implementa método de busca de motoristas

---
+++ src/usecases/driver.usecase.ts
@@ -0,0 +1,15 @@
+import { Driver } from "../interfaces/driver.interface";
+import DriverRepository from "../repositories/driver.repository";
+
+class DriverUseCase {
+    private driverRepository: DriverRepository
+    constructor() {
+        this.driverRepository = new DriverRepository();
+    }
+
+    async findAllDrivers(): Promise<Driver[]> {
+        const drivers = this.driverRepository.findAll();
+
+        return drivers;
+    }
+}
