7520426e18d4de9181cc352b670deba3fe921312
yurioneix <yurioneix18@gmail.com>
2024-11-25 14:42:40 -0300

    Finaliza lógica da rota /ride/estimate

--- src/routes/driver.route.ts
+++ src/routes/driver.route.ts
@@ -1,23 +1,45 @@
 import { FastifyInstance } from "fastify";
 import DriverUseCase from "../usecases/driver.usecase";
 import { User } from "../interfaces/user.interface";
+import { bodySchema } from "../validations/body.schema";
+import getRoute from "../utils/apiRoutes";
+import { parseLatLng } from "../utils/parseStringToLatLong";
 
 export async function driverRoutes(fastify: FastifyInstance) {
     const driverUseCase = new DriverUseCase();
 
-    fastify.post<{Body: User }>('/ride/estimate', (req, reply) => {
+    fastify.post<{Body: User }>('/ride/estimate', {
+        schema: {
+            body: bodySchema,
+        },
+        preValidation: async (req, reply) => {
+            const { origin, destination } = req.body;
 
-        const { customerId, origin, destination } = req.body;
-
-        try {
-            const drivers = driverUseCase.findAllDrivers();
-        } catch (error) {
-            reply.send(error);
+            if (origin === destination) {
+                reply.status(400).send({
+                    error_code: "INVALID_DATA",
+                    error_description: "Os dados fornecidos no corpo da requisição são inválidos"
+                })
+            }
         }
+    },
+    async (req, reply) => {
+
+        const { origin, destination } = req.body;
+
+        const bodyOrigin = parseLatLng(origin);
+        const bodyDestination = parseLatLng(destination);
+
+        if (bodyOrigin && bodyDestination) {
+            const routesResponse = await getRoute(bodyOrigin, bodyDestination)
+
+            reply.status(200).send(routesResponse);
+        }
+
     });
 
     fastify.get('/', (req, reply) => {
         const drivers = driverUseCase.findAllDrivers();
-        return drivers;
+        return drivers; 
     })
 }
