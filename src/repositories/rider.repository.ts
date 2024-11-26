aa614e47ae748c26eacc40ded88d6ce76ca16d1b
yurioneix <yurioneix18@gmail.com>
2024-11-26 15:50:58 -0300

    Implementa repository de ride

--- src/repositories/rider.repository.ts
+++ src/repositories/rider.repository.ts
@@ -1,1 +1,10 @@
 import { prisma } from "../database/prisma-client";
+import { Ride, IRideRepository } from "../interfaces/ride.interface";
+
+export class RideRepositoy implements IRideRepository {
+    async create(ride: Ride): Promise<void> {
+        const newRide = await prisma.ride.create({
+            data: ride,
+        });
+    }
+}
