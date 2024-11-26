e37b2d77bdc336de8b6977896cec5765ddfd661f
yurioneix <yurioneix18@gmail.com>
2024-11-25 23:22:30 -0300

    Finaliza lógica da rota POST /ride/estimate

--- src/routes/driver.route.ts
+++ src/routes/driver.route.ts
@@ -24,16 +24,36 @@ export async function driverRoutes(fastify: FastifyInstance) {
         }
     },
     async (req, reply) => {
-
         const { origin, destination } = req.body;
+
+        const drivers = await driverUseCase.findAllDrivers();
+        const changeReviewDrivers = drivers.map(driver => {
+            const [rating, ...comments] = driver.review.split(/\/5\s+/);
+            return {
+                ...driver,
+                review: {
+                    rating: parseInt(rating, 10),
+                    comment: comments.join(' ')
+                }
+            }
+        })
 
         const bodyOrigin = parseLatLng(origin);
         const bodyDestination = parseLatLng(destination);
 
         if (bodyOrigin && bodyDestination) {
             const routesResponse = await getRoute(bodyOrigin, bodyDestination)
+            const distanceMeters = routesResponse.routes[0].distanceMeters;
+            const duration = routesResponse.routes[0].duration;
 
-            reply.status(200).send(routesResponse);
+            reply.status(200).send({
+                origin: bodyOrigin,
+                destination: bodyDestination,
+                distance: distanceMeters,
+                duration,
+                options: changeReviewDrivers,
+                routeResponse: routesResponse
+            });
         }
 
     });
@@ -41,5 +61,5 @@ export async function driverRoutes(fastify: FastifyInstance) {
     fastify.get('/', (req, reply) => {
         const drivers = driverUseCase.findAllDrivers();
         return drivers; 
-    })
+    });
 }
