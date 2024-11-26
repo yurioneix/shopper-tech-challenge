abdff54417b1ba9215a0039b71b7612d6dadffe3
yurioneix <yurioneix18@gmail.com>
2024-11-26 17:26:19 -0300

    Cria usecase para ride

---
+++ src/usecases/ride.usecase.ts
@@ -0,0 +1,16 @@
+import { Ride } from "../interfaces/ride.interface";
+import { RideRepository } from "../repositories/rider.repository";
+
+export class RideUseCase {
+    private rideRepository: RideRepository;
+
+    constructor() {
+        this.rideRepository = new RideRepository();
+    }
+
+    async createRide(ride: Ride): Promise<Ride> {
+        const newRide = await this.rideRepository.create(ride);
+
+        return newRide;
+    }
+}
