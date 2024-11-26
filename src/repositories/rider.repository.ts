e2d659805fe10a3a1cad318ee56a5bb624723a3c
yurioneix <yurioneix18@gmail.com>
2024-11-26 16:13:45 -0300

    Implementa função create no repository de ride

--- src/repositories/rider.repository.ts
+++ src/repositories/rider.repository.ts
@@ -2,9 +2,11 @@ import { prisma } from "../database/prisma-client";
 import { Ride, IRideRepository } from "../interfaces/ride.interface";
 
 export class RideRepositoy implements IRideRepository {
-    async create(ride: Ride): Promise<void> {
+    async create(ride: Ride): Promise<Ride> {
         const newRide = await prisma.ride.create({
             data: ride,
         });
+
+        return newRide;
     }
 }
