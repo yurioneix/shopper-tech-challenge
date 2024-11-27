d40acd3ef819217d61b960b473673a828d953df2
yurioneix <yurioneix18@gmail.com>
2024-11-27 17:51:01 -0300

    Implementa méetodo getRidesByCustomerId na useCase de Ride

--- src/usecases/ride.usecase.ts
+++ src/usecases/ride.usecase.ts
@@ -1,4 +1,4 @@
-import { BodyRide } from "../interfaces/ride.interface";
+import { BodyRide, RideComplete } from "../interfaces/ride.interface";
 import { RideRepository } from "../repositories/ride.repository";
 
 export class RideUseCase {
@@ -12,5 +12,10 @@ export class RideUseCase {
         const newRide = await this.rideRepository.create(ride);
 
         return newRide;
+    }
+
+    async getRidesByCustomerId(customerId: number, driverId?: number): Promise<RideComplete[]> {
+        const getRides = await this.rideRepository.getRidesByCustomerId(customerId, driverId);
+        return getRides;
     }
 }
