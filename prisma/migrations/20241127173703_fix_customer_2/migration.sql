7c0a592de7ff8d56380c96856d2e3c9c3baec123
yurioneix <yurioneix18@gmail.com>
2024-11-27 14:41:39 -0300

    Adiciona campo Date e altera chave primaria composta

---
+++ prisma/migrations/20241127173703_fix_customer_2/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The primary key for the `rides` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterTable
+ALTER TABLE "rides" DROP CONSTRAINT "rides_pkey",
+ADD CONSTRAINT "rides_pkey" PRIMARY KEY ("driverId", "customerId");
